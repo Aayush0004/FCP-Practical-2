e719483e3ec8344b49ed9c9b991be94d382633f4
Aayush0004 <149601246+Aayush0004@users.noreply.github.com>
2023-12-25 22:43:42 +0530

    Question-7 c.c

---
+++ Question-7 c.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+void main()
+{
+    FILE *fp;
+    char data[100];
+
+    fp = fopen("Practical.txt", "a");
+
+    printf("Enter data to append in the file: \n");
+    gets(data);
+
+    fprintf(fp, "%s", data);
+    fclose(fp);
+
+    printf("Data appended to the file successfully");
+}
